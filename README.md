799e9f1d53c80fc061ff9de9dee24b69e4796bd7
Ahmad Chughtai <ahmadchughtai21@gmail.com>
2024-02-22 15:57:13 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TimeConverter-in-Cpp
+ This C++ program converts the input number of days into hours.
---
+++ TimeConverter-in-Cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int days;
+    cout << "Enter days : ";
+    cin >> days;
+
+    cout << days << " days have " << days * 24 << " hours!";
+    return 0;
+}
